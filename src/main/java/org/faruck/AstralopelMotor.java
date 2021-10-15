05fd37caefa509489165a9afb11c1203f117ce46
faruckchavarria <evafaruck@gmail.com>
2021-10-15 14:08:31 -0600

    se crea la clase correspondiente a AstralopelMotor que contiene informacion sobre su motor

---
+++ src/main/java/org/faruck/AstralopelMotor.java
@@ -0,0 +1,13 @@
+package org.faruck;
+
+public class AstralopelMotor extends Motor {
+
+@Override
+    public String mostrarmotor()
+{
+    System.out.println("Descripcion \n");
+    String motor ="Marca Astral opel, con motor de 4 cilindros, 3.2, a 400 caballos de fuerza y 350 newton metro";
+    return motor;
+}
+
+}
